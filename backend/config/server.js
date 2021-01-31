ce616388a252af51c7e3b2fe884b92f9d09935b2
andrecio <andreciobezerra@gmail.com>
2021-01-31 16:22:21 -0300

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '064d4d40815bf24d18717f269fa2d825'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7efc1b6-f38d-44b4-a62f-21dc0b90df82'
+};
